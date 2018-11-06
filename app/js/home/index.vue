ce4d0291953b3a5a2bee1bbdef88162a1bf346af
unknown <you@example.com>
2018-11-07 03:00:24 +0800

    haha

---
+++ app/js/home/index.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+    <div>
+        <Heador></Heador>
+        <Slider></Slider>
+        
+    </div>
+</template>
+
+<script>
+import Heador from '../public/header'
+import Slider from './hslider'
+export default {
+  components: {
+    Heador,Slider
+  }
+}
+</script>
+
+
---
+++ app/js/home/novice.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Panel title="新手特权" :class="$style.panel">
+            <section :class="$style.content">
+                <div class="item">a</div>
+                <div class="item">
+                    <ul>
+                        <li>c</li>
+                        <li>d</li>
+                    </ul>
+                </div>
+            </section>
+        </Panel>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    components: {}
+};
+</script>
+
+<style>
+</style>
