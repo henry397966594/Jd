0d171412cca108359da8e708505418c661e402a4
henry397966594 <18618415652@163.com>
2018-11-13 00:11:19 +0800

    首页完成

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -6,6 +6,9 @@
         <Borrow></Borrow>
         <Money></Money>
         <Product></Product>
+        <Life></Life>
+        <Footor></Footor>
+        <Navbar></Navbar>
         
     </div>
 </template>
@@ -17,9 +20,12 @@ import Novice from './novice'
 import Borrow from './borrow'
 import Money from './money'
 import Product from './product'
+import Life from './life'
+import Footor from '../public/footer'
+import Navbar from '../public/navbar'
 export default {
   components: {
-    Heador,Slider,Novice,Borrow,Money,Product
+    Heador,Slider,Novice,Borrow,Money,Product,Life,Footor,Navbar
   }
 }
 </script>
